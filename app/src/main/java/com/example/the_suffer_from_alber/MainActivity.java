7d62a73d89093b17b65004c9e6ca489890f8f223
Nothing Nothing <nothing@nothing>
2025-03-31 14:29:07 +0300

    marrage

--- app/src/main/java/com/example/the_suffer_from_alber/MainActivity.java
+++ app/src/main/java/com/example/the_suffer_from_alber/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.the_suffer_from_alber;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +14,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    WebView web;
+    Button btn;
+    EditText ed;
+    String adress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        web = findViewById(R.id.web);
+        WebSettings webSettings = web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        web.loadUrl("https://www.google.co.in");
+
+        btn = findViewById(R.id.btn);
+        ed = findViewById(R.id.ed);
+
+    }
+
+    public void onPush(View view) {
+
     }
 }
